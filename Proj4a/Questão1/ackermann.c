ae33b29d77edecc71d8f245fd586414af47802b1
Maira Gomes <96553487+mairagomes@users.noreply.github.com>
2024-06-08 15:05:49 -0300

    modificando codigo

--- Proj4a/Questão1/ackermann.c
+++ Proj4a/Questão1/ackermann.c
@@ -1,6 +1,7 @@
 //Aluna: Maira Gomes
 
 #include <stdio.h>
+#include <time.h>
 
 // Função de Ackermann
 unsigned int ackermann(unsigned int m, unsigned int n) {
@@ -14,9 +15,27 @@ unsigned int ackermann(unsigned int m, unsigned int n) {
 }
 
 int main() {
-    unsigned int m, n;
-    printf("Digite os valores de m e n: ");
-    scanf("%u %u", &m, &n);
-    printf("Ackermann(%u, %u) = %u\n", m, n, ackermann(m, n));
+    unsigned int m, n, result;
+    clock_t start, end;
+    double cpu_time_used;
+
+    start = clock();
+
+    printf("Calculando valores da função de Ackermann que podem ser computados em menos de 1 minuto...\n");
+
+    for (m = 0; m <= 3; ++m) { // Mantenha m pequeno devido ao crescimento rápido
+        for (n = 0; n <= 10; ++n) { // Ajuste n conforme necessário
+            end = clock();
+            cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+            if (cpu_time_used >= 60.0) {
+                printf("Tempo limite atingido.\n");
+                return 0;
+            }
+            result = ackermann(m, n);
+            printf("Ackermann(%u, %u) = %u\n", m, n, result);
+        }
+    }
+
     return 0;
 }
+
