7a6cbdd961e9410c4467ce74cfbdbe61cfaa7967
Maira Gomes <96553487+mairagomes@users.noreply.github.com>
2024-06-08 15:06:48 -0300

    modificando codigo

--- Proj4a/Questão1/ackermann.lisp
+++ Proj4a/Questão1/ackermann.lisp
@@ -2,3 +2,13 @@
   (cond ((= m 0) (+ n 1))
         ((and (> m 0) (= n 0)) (ackermann (- m 1) 1))
         (t (ackermann (- m 1) (ackermann m (- n 1))))))
+
+(defun calcular-ackermann ()
+  (let ((start-time (get-internal-real-time)))
+    (loop for m from 0 to 3 do ; Mantenha M pequeno devido ao crescimento rápido
+          (loop for n from 0 to 10 do ; Ajuste N conforme necessário
+                (let ((current-time (get-internal-real-time)))
+                  (when (>= (/ (- current-time start-time) internal-time-units-per-second) 60)
+                    (return (format t "Tempo limite atingido.~%"))))
+                (let ((resultado (ackermann m n)))
+                  (format t "Ackermann(~d, ~d) = ~d~%" m n resultado))))))
