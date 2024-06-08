803b0de242c5319434489df01aec3422aefd2ebc
Maira Gomes <96553487+mairagomes@users.noreply.github.com>
2024-06-08 15:07:09 -0300

    modificando codigo

--- Proj4a/Questão1/ackermann.pl
+++ Proj4a/Questão1/ackermann.pl
@@ -11,3 +11,15 @@ ackermann(M, N, Resultado) :-
     N1 is N - 1,
     ackermann(M, N1, ResultadoTemp),
     ackermann(M1, ResultadoTemp, Resultado).
+
+% Calcular todos os valores possíveis em menos de 1 minuto
+calcular_ackermann :-
+    statistics(runtime, [T0|_]),
+    between(0, 3, M), % Mantenha M pequeno devido ao crescimento rápido
+    between(0, 10, N), % Ajuste N conforme necessário
+    statistics(runtime, [T1|_]),
+    (T1 - T0) < 60000,
+    ackermann(M, N, Resultado),
+    format('Ackermann(~d, ~d) = ~d~n', [M, N, Resultado]),
+    fail.
+calcular_ackermann.
