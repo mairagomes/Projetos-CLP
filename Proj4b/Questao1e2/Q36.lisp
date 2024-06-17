90b04e85e6b65a11f8119d3f3eb7c637e6652c02
Maira Gomes <96553487+mairagomes@users.noreply.github.com>
2024-06-17 20:43:26 -0300

    add arquivo

---
+++ Proj4b/Questao1e2/Q36.lisp
@@ -0,0 +1,22 @@
+(defun fatorial (n)
+ (cond
+ ((< n 0) nil)
+ ((zerop n) 1)
+ (T (* n (fatorial (- n 1))))
+ )
+)
+(defun fibonacci (n)
+ (cond
+ ((< n 1) nil)
+ ((< n 3) 1)
+ (T (+ (fibonacci (- n 1)) (fibonacci (- n 2))))
+ )
+)
+(defun serieG (n)
+ (cond
+ ((< n 1) nil)
+ ((eql n 1) 1)
+ ((eql (mod n 2) 0) (- (serieG (- n 1))(/ n (fatorial (fibonacci n)))))
+ (T (+ (serieG (- n 1))(/ n (fatorial (fibonacci n)))))
+ )
+)
